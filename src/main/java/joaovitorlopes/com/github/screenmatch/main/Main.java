e165859d19aa0a1641acd0eab97b24ce1a3fc9e0
joaovitorlopes <jvlaaa+github@gmail.com>
2024-05-13 20:56:33 -0400

    adding feature to show menu option and adding methods to search series and episodes by series

--- src/main/java/joaovitorlopes/com/github/screenmatch/main/Main.java
+++ src/main/java/joaovitorlopes/com/github/screenmatch/main/Main.java
@@ -1,16 +1,11 @@
 package joaovitorlopes.com.github.screenmatch.main;
 
-import joaovitorlopes.com.github.screenmatch.model.EpisodeData;
-import joaovitorlopes.com.github.screenmatch.model.Episode;
 import joaovitorlopes.com.github.screenmatch.model.SeriesData;
 import joaovitorlopes.com.github.screenmatch.service.ConsumeAPI;
 import joaovitorlopes.com.github.screenmatch.service.DataConversion;
 import joaovitorlopes.com.github.screenmatch.model.SeasonData;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
 
@@ -21,59 +16,69 @@ public class Main {
     private final String ADDRESS = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=5c08242";
 
+    private List<SeriesData> seriesData = new ArrayList<>();
+
     public void showMenu() {
+        var option = -1;
+        while (option != 0) {
+            var menu = """
+                    1 - Search Series
+                    2 - Search Episodes
+                    3 - List Searched Series
+            
+                    0 - Exit
+                    """;
+
+            System.out.println(menu);
+            option = reading.nextInt();
+            reading.nextLine();
+
+            switch (option) {
+                case 1:
+                    searchSeriesWeb();
+                    break;
+                case 2:
+                    searchEpisodeBySeries();
+                    break;
+                case 3:
+                    listSearchedSeries();
+                    break;
+                case 0:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid option!");
+            }
+        }
+    }
+
+    private void searchSeriesWeb() {
+        SeriesData data = getSeriesData();
+        seriesData.add(data);
+        System.out.println(data);
+    }
+
+    private SeriesData getSeriesData() {
         System.out.println("Enter a series name to search:");
         var seriesName = reading.nextLine();
         var json = consumeAPI.getData(ADDRESS + seriesName.replace(" ", "+") + API_KEY);
         SeriesData seriesData = conversion.getData(json, SeriesData.class);
-        System.out.println(seriesData);
+        return seriesData;
+    }
 
+    private void searchEpisodeBySeries() {
+        SeriesData seriesData = getSeriesData();
         List<SeasonData> seasons = new ArrayList<>();
 
-		for (int i = 1; i <= seriesData.totalSeasons(); i++) {
-			json = consumeAPI.getData(ADDRESS + seriesName.replace(" ", "+") + "&season=" + i + API_KEY);
-			SeasonData seasonData = conversion.getData(json, SeasonData.class);
-			seasons.add(seasonData);
-		}
+        for (int i = 1; i <= seriesData.totalSeasons(); i++) {
+            var json = consumeAPI.getData(ADDRESS + seriesData.title().replace(" ", "+") + "&season=" + i + API_KEY);
+            SeasonData seasonData = conversion.getData(json, SeasonData.class);
+            seasons.add(seasonData);
+        }
+        seasons.forEach(System.out::println);
+    }
 
-		seasons.forEach(System.out::println);
-        System.out.println("----EPISODES NAME----");
-        seasons.forEach(s -> s.episodes().forEach(e -> System.out.println(e.title())));
-        System.out.println("----EPISODES NAME----");
-
-        List<EpisodeData> episodeData = seasons.stream()
-                .flatMap(s -> s.episodes().stream())
-                .collect(Collectors.toList());
-
-        System.out.println("----TOP 5 EPISODES----");
-        episodeData.stream()
-                .filter(e -> !e.rating().equalsIgnoreCase("N/A"))
-                .sorted(Comparator.comparing(EpisodeData::rating).reversed())
-                .limit(5)
-                .forEach(System.out::println);
-        System.out.println("----TOP 5 EPISODES----");
-
-        List<Episode> episodes = seasons.stream()
-                .flatMap(s -> s.episodes().stream()
-                        .map(d -> new Episode(s.season(), d))
-                ).collect(Collectors.toList());
-
-        episodes.forEach(System.out::println);
-
-        System.out.println("Enter a year would you like to view the episodes:");
-        var year = reading.nextInt();
-        reading.nextLine();
-
-        LocalDate searchDate = LocalDate.of(year, 1, 1);
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-        episodes.stream()
-                .filter(e -> e.getReleasedData() != null && e.getReleasedData().isAfter(searchDate))
-                .forEach(e -> System.out.println(
-                        "Season: " + e.getSeason() +
-                                " Episode: " + e.getTitle() +
-                                " ReleasedData: " + e.getReleasedData().format(formatter)
-                ));
+    private void listSearchedSeries() {
+        seriesData.forEach(System.out::println);
     }
 }
